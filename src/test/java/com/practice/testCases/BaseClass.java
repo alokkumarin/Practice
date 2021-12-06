7469e6a264ddd84aa1dcb0c2d8a20f6a77427377
Alok Kumar <alokkumar.in@gmail.com>
2021-12-06 12:48:53 +0530

    Added some sysout in project...

--- src/test/java/com/practice/testCases/BaseClass.java
+++ src/test/java/com/practice/testCases/BaseClass.java
@@ -8,5 +8,6 @@ public class BaseClass {
 		// TODO Auto-generated constructor stub
 		PropertyConfigurator.configure("log4j.properties");
 		System.out.println("i am add some point Here");
+		System.out.println(" I added");
 	}
 }
